1a6611b8baf377c6b52f6db65c6fefb70d94345f
Pranesh0908 <168210455+Pranesh0908@users.noreply.github.com>
2024-04-28 14:05:10 +0530

    Add files via upload

---
+++ 1b.py
@@ -0,0 +1,9 @@
+#Swap two numbers with temp
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+temp=a
+a=b
+b=temp
+print("After Swapping")
+print("Value of a:",a)
+print("Value of b:",b)
